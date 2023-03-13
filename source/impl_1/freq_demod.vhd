6a4d5a677b99e7383cf7b5fab97e620537716932
Wojciech Kaczmarski <44336093+sp5wwp@users.noreply.github.com>
2023-03-13 13:15:29 +0100

    Update freq_demod.vhd
    freq demod block update

--- source/impl_1/freq_demod.vhd
+++ source/impl_1/freq_demod.vhd
@@ -20,10 +20,11 @@ end freq_demod;
 architecture magic of freq_demod is
 	type delay_line is array(0 to 1) of signed(15 downto 0);
 	signal dly_i, dly_q : delay_line := (others => (others => '0'));
-	signal a_i, a_q : signed(15 downto 0) := (others => '0'); -- is 16 bits enough?
-	signal m_i, m_q, diff : signed(31 downto 0) := (others => '0');
-	signal sum_sq : signed(31 downto 0) := (0 => '1', others => '0');
-	signal res : signed(31 downto 0) := (others => '0');
+	signal a_i, a_q : signed(16 downto 0) := (others => '0');
+	signal m_i, m_q : signed(32 downto 0) := (others => '0');
+	signal diff : signed(33 downto 0) := (others => '0');
+	signal sum_sq : signed(32 downto 0) := (others => '0');
+	signal res : signed(33 downto 0) := (others => '0');
 begin
 	process(clk_i)
 	begin
@@ -33,19 +34,19 @@ begin
 			dly_i(0) <= i_i;
 			dly_q(1) <= dly_q(0);
 			dly_q(0) <= q_i;
+			demod_o <= res(15 downto 0);
 		end if;
 	end process;
 	
 	-- sum of squares
-	sum_sq <= i_i*i_i + q_i*q_i;
+	sum_sq <= resize(i_i*i_i, 33) + q_i*q_i;
 	-- add blocks
-	a_i <= i_i - dly_i(1);
-	a_q <= q_i - dly_q(1);
+	a_i <= resize(i_i, 17) - dly_i(1);
+	a_q <= resize(q_i, 17) - dly_q(1);
 	-- multiply blocks
 	m_i <= dly_i(0) * a_q;
 	m_q <= dly_q(0) * a_i;
 	-- result
-	diff <= m_i - m_q;
-	res <= diff / sum_sq(31 downto 16);
-	demod_o <= res(16 downto 1);
+	diff <= resize(m_i, 34) - m_q;
+	res <= diff / sum_sq(32 downto 32-19+1) when signed(sum_sq(32 downto 32-19+1))/=0 else (others => '0');
 end magic;
