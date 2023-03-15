c61780885bbe9a2c9a123f168eb0e3981e0d1bb6
Wojciech Kaczmarski <44336093+sp5wwp@users.noreply.github.com>
2023-03-15 11:53:41 +0100

    typo fix

--- source/impl_1/iq_balancer.vhd
+++ source/impl_1/iq_balancer.vhd
@@ -17,8 +17,8 @@ entity iq_balancer_16 is
 		q_i		: in std_logic_vector(15 downto 0);			-- Q data in
 		ib_i	: in std_logic_vector(15 downto 0);			-- I balance in, 0x4000 = "+1.0"
 		qb_i	: in std_logic_vector(15 downto 0);			-- Q balance in, 0x4000 = "+1.0"
-		i_o		: out std_logic_vector(15 downto 0);		-- I data in
-		q_o		: out std_logic_vector(15 downto 0)			-- Q data in
+		i_o		: out std_logic_vector(15 downto 0);		-- I data out
+		q_o		: out std_logic_vector(15 downto 0)			-- Q data out
 	);
 end iq_balancer_16;
 
