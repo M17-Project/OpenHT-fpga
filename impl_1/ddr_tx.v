f761268e0c818c75dfae491dd48441399e7834f3
Wojciech Kaczmarski <44336093+sp5wwp@users.noreply.github.com>
2023-05-12 19:05:04 +0200

    Add files via upload

--- impl_1/ddr_tx.v
+++ impl_1/ddr_tx.v
@@ -1,5 +1,5 @@
 // Verilog netlist produced by program LSE 
-// Netlist written on Fri May 12 16:03:46 2023
+// Netlist written on Fri May 12 18:59:09 2023
 // Source file index table: 
 // Object locations will have the form @<file_index>(<first_ line>[<left_column>],<last_line>[<right_column>])
 // file 0 "c:/radiant/ip/avant/fifo/rtl/lscc_fifo.v"
