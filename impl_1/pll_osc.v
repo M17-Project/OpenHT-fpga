af688f3325f129e1f6388bc50fd0689484fbe665
Wojciech Kaczmarski <44336093+sp5wwp@users.noreply.github.com>
2023-05-16 21:33:45 +0200

    Add files via upload

--- impl_1/pll_osc.v
+++ impl_1/pll_osc.v
@@ -1,5 +1,5 @@
 // Verilog netlist produced by program LSE 
-// Netlist written on Mon May 15 14:47:52 2023
+// Netlist written on Tue May 16 21:26:16 2023
 // Source file index table: 
 // Object locations will have the form @<file_index>(<first_ line>[<left_column>],<last_line>[<right_column>])
 // file 0 "c:/radiant/ip/avant/fifo/rtl/lscc_fifo.v"
