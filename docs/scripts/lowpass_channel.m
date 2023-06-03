b720007bd46b4c52a46143ecf4227ee4c8efbcad
Wojciech Kaczmarski <44336093+sp5wwp@users.noreply.github.com>
2023-06-03 14:00:51 +0200

    Add files via upload

--- docs/scripts/lowpass_channel.m
+++ docs/scripts/lowpass_channel.m
@@ -6,23 +6,72 @@
 clear;
 clc;
 
-n=80;   %order
+%----------------------------- 6.25k -----------------------------%
+n=80;       %order
+fc=6.25/2;  %channel bw
+a=0.2;      %bw excess
 
-b=firpm(n, [0, 6.25/25, 7.5/25, 1], [1, 1, 0, 0], [1, 1]);
-b = b * (1.0/max(b));
-fvtool(b);
+b0=firpm(n, [0, (fc*(1-a))/25, fc*(1+a)/25, 1], [1, 1, 0, 0], [1, 1]);
+b0 = b0 * (1.0/max(b0))/4;
 
 %convert the taps to VHDL array
 one=double(0x7FFF);
-fprintf('\ttype coefficients is array(0 to NUM_TAPS-1) of signed(15 downto 0);\nsignal coeff_s: coefficients := (\n');
+fprintf('%1.2fk channel filter taps:\n', fc*2);
 for i=1:4:n+1
     if(i<n+1)
-        fprintf('\t\tx\"%04X\", x\"%04X\", x\"%04X\", x\"%04X\",\n', typecast(int16(b(i)*one),'uint16'), ...
-            typecast(int16(b(i+1)*one),'uint16'), ...
-            typecast(int16(b(i+2)*one),'uint16'), ...
-            typecast(int16(b(i+3)*one),'uint16'))
+        fprintf('\t\tx\"%04X\", x\"%04X\", x\"%04X\", x\"%04X\",\n', typecast(int16(b0(i)*one),'uint16'), ...
+            typecast(int16(b0(i+1)*one),'uint16'), ...
+            typecast(int16(b0(i+2)*one),'uint16'), ...
+            typecast(int16(b0(i+3)*one),'uint16'))
     else
-        fprintf('\t\tx\"%04X\"\n', typecast(int16(b(i)*one),'uint16'))
+        fprintf('\t\tx\"%04X\"\n', typecast(int16(b0(i)*one),'uint16'))
     end
 end
-fprintf('\t);\n');
+fprintf('\n');
+
+%----------------------------- 12.5k -----------------------------%
+n=80;       %order
+fc=12.5/2;  %channel bw
+a=0.1;      %bw excess
+
+b1=firpm(n, [0, (fc*(1-a))/25, fc*(1+a)/25, 1], [1, 1, 0, 0], [1, 1]);
+b1 = b1 * (1.0/max(b1))/2;
+
+%convert the taps to VHDL array
+one=double(0x7FFF);
+fprintf('%1.2fk channel filter taps:\n', fc*2);
+for i=1:4:n+1
+    if(i<n+1)
+        fprintf('\t\tx\"%04X\", x\"%04X\", x\"%04X\", x\"%04X\",\n', typecast(int16(b1(i)*one),'uint16'), ...
+            typecast(int16(b1(i+1)*one),'uint16'), ...
+            typecast(int16(b1(i+2)*one),'uint16'), ...
+            typecast(int16(b1(i+3)*one),'uint16'))
+    else
+        fprintf('\t\tx\"%04X\"\n', typecast(int16(b1(i)*one),'uint16'))
+    end
+end
+fprintf('\n');
+
+%----------------------------- 25k -----------------------------%
+n=80;       %order
+fc=25/2;    %channel bw
+a=0.05;     %bw excess
+
+b2=firpm(n, [0, (fc*(1-a))/25, fc*(1+a)/25, 1], [1, 1, 0, 0], [1, 1]);
+b2 = b2 * (1.0/max(b2));
+
+%convert the taps to VHDL array
+one=double(0x7FFF);
+fprintf('%1.2fk channel filter taps:\n', fc*2);
+for i=1:4:n+1
+    if(i<n+1)
+        fprintf('\t\tx\"%04X\", x\"%04X\", x\"%04X\", x\"%04X\",\n', typecast(int16(b2(i)*one),'uint16'), ...
+            typecast(int16(b2(i+1)*one),'uint16'), ...
+            typecast(int16(b2(i+2)*one),'uint16'), ...
+            typecast(int16(b2(i+3)*one),'uint16'))
+    else
+        fprintf('\t\tx\"%04X\"\n', typecast(int16(b2(i)*one),'uint16'))
+    end
+end
+
+fvtool(b0, [1], b1, [1], b2, [1]);
