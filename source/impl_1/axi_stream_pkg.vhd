2ca201c93f5dfad918575922615725fe492bbd7b
Sebastien Van Cauwenberghe <svancau@gmail.com>
2023-07-16 17:08:19 +0300

    Add null AXIS entry

--- source/impl_1/axi_stream_pkg.vhd
+++ source/impl_1/axi_stream_pkg.vhd
@@ -22,6 +22,7 @@ package axi_stream_pkg is
         tvalid : std_logic; -- Data is valid
         tlast : std_logic; -- Last burst of packet
     end record;
+    constant axis_in_iq_null : axis_in_iq_t := ((others => '0'), '0', '0');
 
     type axis_out_iq_t is record
         tready : std_logic; -- Downstream is ready
@@ -32,6 +33,7 @@ package axi_stream_pkg is
         tvalid : std_logic; -- Data is valid
         tlast : std_logic; -- Last burst of packet
     end record;
+    constant axis_in_mod_null : axis_in_mod_t := ((others => '0'), '0', '0');
 
     type axis_out_mod_t is record
         tready : std_logic; -- Downstream is ready
