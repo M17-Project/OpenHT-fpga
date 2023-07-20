e51e73335261f6179d7cf64ded1d00bbe702bf93
Sebastien Van Cauwenberghe <svancau@gmail.com>
2023-07-20 12:27:54 +0300

    Update interpolator sim

--- dsp_sim/interpol_5.py
+++ dsp_sim/interpol_5.py
@@ -5,7 +5,7 @@ from scipy.fft import fft
 from scipy import signal
 
 fs = 8000
-freq = 3200
+freq = 10
 L = 5
 x = np.arange(2048)
 incoming_sig = np.sin(2 * np.pi * freq * x/fs)
@@ -22,8 +22,11 @@ plt.plot(sig_upsampled)
 plt.subplot(4,2,4)
 plt.plot(20*np.log(fft(sig_upsampled)))
 
-fir = signal.remez(400, [0, 3700, 4300, fs*L/2], [1,0], fs=fs*L)
+fir = signal.remez(600, [0, 3800, 4200, fs*L/2], [1,0], fs=fs*L) * L
 plt.subplot(4,2,5)
+plt.plot(fir)
+
+plt.subplot(4,2,6)
 w,h = signal.freqz(fir)
 plt.plot(w, 20*np.log(h))
 
@@ -34,4 +37,4 @@ plt.plot(interpol)
 plt.subplot(4,2,8)
 plt.plot(20*np.log(fft(interpol)))
 
-plt.show() 
+plt.show()
