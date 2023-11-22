61aecfc8ac13e95c2f462d8a710da4758e501afb
fred-corp <7109145+fred-corp@users.noreply.github.com>
2023-11-22 11:45:49 +0100

    added APB interface

--- source/rx_chain/APFM_demodulator.vhd
+++ source/rx_chain/APFM_demodulator.vhd
@@ -12,7 +12,6 @@
 --
 -- TODO : Implement AM and PM demodulation
 -- TODO : Implement mode selector (AM, PM, FM)
--- TODO : Add APB interface
 -- TODO : Implement I/IQ mode for AM
 -- TODO : Normalise phase angle from -pi to pi
 --
@@ -23,13 +22,20 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 use work.axi_stream_pkg.all;
 use work.cordic_pkg.all;
+use work.apb_pkg.all;
 
 entity FM_demodulator is
+  generic  (
+    PSEL_ID : natural
+  );
   port (
     clk_i   : in std_logic;             -- Clock, from upstream
     nrst_i : in std_logic;              -- Reset, from upstream
 
-    s_axis_o : out axis_out_iq_t;       -- slave out, to upstream entity (ready)                      -- This entity ready to receive flag (tready)
+    s_apb_o : out apb_out_t;            -- slave apb interface out, to upstream
+    s_apb_i : in apb_in_t;              -- slave apb interface in, from upstream
+
+    s_axis_o : out axis_out_iq_t;       -- slave out, to upstream entity (ready)                      -- This entity's ready to receive flag (tready)
     s_axis_i : in axis_in_iq_t;         -- slave in, from upstream entity (data and valid)            -- IQ signal (tdata), valid (tvalid)
     m_axis_o : out axis_in_mod_t;       -- master out, to downstream entity (data and valid)          -- Demodulated signal (tdata), valid (tvalid)
     m_axis_i : in axis_out_mod_t        -- master input, from downstream entity (ready)               -- From next entity's ready to receive flag (tready)
@@ -44,6 +50,9 @@ architecture magic of FM_demodulator is
   signal ready : std_logic := '0';
   signal output_valid : std_logic := '0';
   signal cordic_busy : std_logic;
+
+  type demod_mode_t is (AM, PM, FM);
+  signal demod_mode : demod_mode_t := FM;
 
   type sig_state_t is (IDLE, COMPUTE, DONE);
   signal sig_state : sig_state_t := IDLE;
@@ -74,6 +83,36 @@ begin
 
     std_logic_vector(Z_Result) => phase
   );
+
+  -- APB
+  process(clk_i)
+  begin
+    if rising_edge(clk_i) then
+      s_apb_o.pready <= '0';
+      s_apb_o.prdata <= (others => '0');
+
+      if s_apb_i.PSEL(PSEL_ID) then
+        if s_apb_i.PENABLE and s_apb_i.PWRITE then
+          case s_apb_i.PADDR(2 downto 1) is
+            when "00" => -- Mode
+              demod_mode <= demod_mode_t(to_integer(unsigned(s_apb_i.PWDATA(1 downto 0))));
+            when others =>
+              null;
+          end case;
+        end if;
+
+        if not s_apb_i.PENABLE then
+          s_apb_o.pready <= '1';
+          case s_apb_i.PADDR(2 downto 1) is
+            when "00" => -- Mode
+              s_apb_o.prdata <= std_logic_vector(to_unsigned(to_integer(demod_mode), 2));
+            when others =>
+              null;
+          end case;
+        end if;
+      end if;
+    end if;
+  end process;
 
   -- FSM
   process(clk_i)
